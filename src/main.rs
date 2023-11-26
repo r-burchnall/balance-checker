5768e10eb8ef1a85fe80d724a00d364eb4c975fd
Ross Burchnall <r-burchnall@hotmail.co.uk>
2023-11-26 16:27:10 +0000

    create a simple way to retrieve a wallet

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use solana_client::rpc_client::RpcClient;
+use solana_program::pubkey::Pubkey;
+use solana_sdk::commitment_config::CommitmentConfig;
+use std::str::FromStr;
+
 fn main() {
-    println!("Hello, world!");
+    let address = std::env::args()
+        .nth(1)
+        .expect("You must provide an address");
+
+    println!("Fetching balance for address: {}", address);
+
+    let rpc_client = RpcClient::new("https://api.mainnet-beta.solana.com");
+    let pub_key = Pubkey::from_str(&address)
+        .expect("unable to create pubkey");
+
+    let balance = rpc_client
+        .get_balance_with_commitment(&pub_key, CommitmentConfig::processed())
+        .expect("unable to fetch balance");
+
+    let balance = balance.value as f64 / 1e9;
+    println!("{}", balance)
 }
